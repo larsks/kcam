46cb4e96120b31aa98b44724b57eae07cfde7e7c
Lars Kellogg-Stedman <lars@oddbit.com>
2017-09-20 20:11:09 -0400

    added door sensor

--- kcam/main.py
+++ kcam/main.py
@@ -1,5 +1,5 @@
 import logging
-import threading
+import threading  # NOQA
 
 from RPi import GPIO
 from signal import pause
@@ -25,6 +25,38 @@ class LED:
 
     def off(self):
         GPIO.output(self.pin, 0)
+
+
+class DoorSensor:
+
+    def __init__(self, pin=None, on_door_open=None, on_door_close=None):
+        self.pin = int(pin)
+        self.on_door_open = on_door_open
+        self.on_door_close = on_door_close
+
+        self.setup_pin()
+
+    def setup_pin(self):
+        GPIO.setup(self.pin, GPIO.IN)
+
+    def enable(self):
+        LOG.info('enabling door sensor on pin %d', self.pin)
+        GPIO.add_event_detect(self.pin, GPIO.BOTH,
+                              callback=self.handler)
+
+    def disable(self):
+        GPIO.remove_event_detect(self.pin)
+
+    @property
+    def value(self):
+        return GPIO.input(self.pin)
+
+    def handler(self, pin):
+        LOG.debug('door event on pin %d', pin)
+        if self.on_door_open and self.value:
+            self.on_door_open(self)
+        elif self.on_door_close:
+            self.on_door_close(self)
 
 
 class MotionSensor:
@@ -94,6 +126,7 @@ class KCam:
 
     def __init__(self,
                  pir_pin=None,
+                 door_pin=None,
                  pwr_led_pin=None,
                  arm_led_pin=None,
                  det_led_pin=None,
@@ -105,16 +138,25 @@ class KCam:
                  record_limit=300):
 
         self.pir_pin = pir_pin
+        self.door_pin = door_pin
         self.pwr_led = LED(pwr_led_pin)
         self.arm_led = LED(arm_led_pin)
         self.act_led = LED(act_led_pin)
         self.det_led = LED(det_led_pin)
+
         self.motion = MotionSensor(pir_pin,
                                    on_motion=self.on_motion,
                                    on_no_motion=self.on_no_motion)
-
         self.arm_btn = Button(arm_btn_pin,
                               on_press=self.on_arm_button)
+        self.door = DoorSensor(door_pin,
+                               on_door_open=self.on_door_open,
+                               on_door_close=self.on_door_close)
+
+        self.enable_at_startup = [
+            self.arm_btn,
+            self.door,
+        ]
 
         self.active = False
         self.arm_delay = arm_delay
@@ -127,7 +169,9 @@ class KCam:
         self.disarm()
 
     def enable(self):
-        self.arm_btn.enable()
+        for sensor in self.enable_at_startup:
+            sensor.enable()
+
         self.pwr_led.on()
 
     def disable(self):
@@ -200,11 +244,18 @@ class KCam:
         LOG.info('end cooldown period')
         self.cooldown = False
 
+    def on_door_open(self, pin):
+        LOG.info('door open')
+
+    def on_door_close(self, pin):
+        LOG.info('door closed')
+
 
 def main():
     logging.basicConfig(level='DEBUG')
     cam = KCam(pir_pin=23, det_led_pin=24, act_led_pin=25,
-               pwr_led_pin=17, arm_led_pin=27, arm_btn_pin=22)
+               pwr_led_pin=17, arm_led_pin=27, arm_btn_pin=22,
+               door_pin=4)
     cam.enable()
     pause()
 
