e9457789cafe708637604ce879a589e69e984382
Lars Kellogg-Stedman <lars@oddbit.com>
2017-09-24 17:21:48 -0400

    added ansible roles

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='kcam',
@@ -6,11 +6,7 @@ setup(
     author='Lars Kellogg-Stedman',
     author_email='lars@oddbit.com',
     url='https://github.com/larsks/kcam',
-    packages=['kcam'],
-    install_requires=[
-        'RPi.GPIO',
-        'Adafruit-DHT',
-    ],
+    packages=find_packages(),
     entry_points={
         'console_scripts': [
             'kcam = kcam.main:main',
